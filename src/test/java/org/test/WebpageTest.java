5ee38da5073ab57736dc38cbd6515954c6362c78
Theja N <thejan.hdk@gmail.com>
2026-03-02 04:09:30 +0530

    Added complete Gradle project with test files

---
+++ src/test/java/org/test/WebpageTest.java
@@ -0,0 +1,30 @@
+package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class WebpageTest {
+    WebDriver driver;
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://thejan.github.io/Gradle_DemoB2/");
+    }
+    @Test
+    public void titleValidationTest() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Tripillar Solutions";
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(1000);
+
+
+        driver.quit();
+    }
+}
